74766fa372bcb8d5b21dd92d0c0b962a19939378
DevExpressExampleBot <fakeEmail>
2022-12-17 14:45:33 +0400

    autogenerate VB

--- VB/DXSample/ViewModel/MainViewModel.vb
+++ VB/DXSample/ViewModel/MainViewModel.vb
@@ -1,22 +1,23 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.POCO
 Imports DevExpress.Mvvm.DataAnnotations
-Imports System
 Imports System.Linq
 
 Namespace DXSample.ViewModel
-    <POCOViewModel> _
+
+    <POCOViewModel>
     Public Class MainViewModel
-        Protected ReadOnly Property DocumentManagerService() As IDocumentManagerService
+
+        Protected ReadOnly Property DocumentManagerService As IDocumentManagerService
             Get
-                Return Me.GetService(Of IDocumentManagerService)()
+                Return GetService(Of IDocumentManagerService)()
             End Get
         End Property
 
         Public Sub CreateDocument()
             Dim doc As IDocument = DocumentManagerService.CreateDocument("TabView", ViewModelSource.Create(Function() New TabViewModel()))
-            doc.Id = String.Format("DocId_{0}", DocumentManagerService.Documents.Count())
-            doc.Title = String.Format("Item {0}", DocumentManagerService.Documents.Count())
+            doc.Id = String.Format("DocId_{0}", DocumentManagerService.Documents.Count(Of IDocument)())
+            doc.Title = String.Format("Item {0}", DocumentManagerService.Documents.Count(Of IDocument)())
             doc.Show()
         End Sub
 
