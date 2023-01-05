11a4b29ee6ed5d18120ea8fa3346b4c8c2728d74
AndreySlabov <andrey.slabov@devexpress.com>
2023-01-05 14:04:00 +0400

    Update MainViewModel.cs

--- CS/DXSample/ViewModel/MainViewModel.cs
+++ CS/DXSample/ViewModel/MainViewModel.cs
@@ -11,8 +11,8 @@ namespace DXSample.ViewModel {
 
         public void CreateDocument() {
             IDocument doc = DocumentManagerService.CreateDocument("TabView", ViewModelSource.Create(()=> new TabViewModel()));
-            doc.Id = String.Format("DocId_{0}", DocumentManagerService.Documents.Count<IDocument>());
-            doc.Title = String.Format("Item {0}", DocumentManagerService.Documents.Count<IDocument>());
+            doc.Id = String.Format("DocId_{0}", DocumentManagerService.Documents.Count());
+            doc.Title = String.Format("Item {0}", DocumentManagerService.Documents.Count());
             doc.Show();
         }
 
